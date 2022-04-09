e2d1360624765b456cc8e4706b17c37e360af215
TomatoWang233 <99958184+TomatoWang233@users.noreply.github.com>
2022-04-09 16:27:49 +0800

    服务器测试通过

--- main.py
+++ main.py
@@ -17,9 +17,10 @@ global CHANNEL_URL
 TOKEN=""
 APP_ID=""
 APP_SECRET=""
-CHANNEL=["教务信息"]
-CHANNEL_ID={"教务信息":0}
-CHANNEL_URL={"教务信息":"www.baidu.com"}
+
+CHANNEL=["本科生院-教务公告通知","本科生院-教务教学动态","研究生院-动态通知"]
+CHANNEL_ID={"本科生院-教务公告通知":102,"本科生院-教务教学动态":103,"研究生院-动态通知":104}
+CHANNEL_URL={"本科生院-教务公告通知":"https://rss.shab.fun/nju/jw/ggtz","本科生院-教务教学动态":"https://rss.shab.fun/nju/jw/jxdt","研究生院-动态通知":"https://rss.shab.fun/nju/gra"}
 # ===
 logger.info("booting njuhelper-x ...")
 logger.info("starting scheduler ...")
@@ -44,7 +45,7 @@ def add(message,session,match):
     ch=match.group(1)
     if ch not in CHANNEL:
         return "该频道不存在！"
-    client.tag_users([user],CHANNEL_ID[ch])
+    client.tag_users(str(CHANNEL_ID[ch]),[user])
     return "订阅成功"
 
 @robot.filter(re.compile("退订 (.*)"))
@@ -53,12 +54,16 @@ def dele(message,session,match):
     ch=match.group(1)
     if ch not in CHANNEL:
         return "该频道不存在！"
-    client.untag_users([user],CHANNEL_ID[ch])
-    return "订阅成功"
+    client.untag_users(CHANNEL_ID[ch],[user])
+    return "退订成功"
 
 @robot.filter("about")
 def about(message,session,match):
     return "njuhelper-x by TomatoWang"
+
+@robot.filter("列表")
+def list(message,session,match):
+    return "【频道列表】\n"+"\n".join(CHANNEL)
 
 @robot.subscribe
 def subscribe(message):
@@ -71,9 +76,9 @@ for i in CHANNEL:
 for i in rss_sources:
     i.bind(robot)
     i.run()
+    logger.success("rss source:"+i.name+" ready")
 logger.success("rss ready")
+
 logger.info("all done, starting robot...")
 
-application = tornado.web.Application([(r"/robot/", make_handler(robot)),])
-application.listen(8888)
-tornado.ioloop.IOLoop.instance().start()
+robot.run(server='tornado')
--- rss_source.py
+++ rss_source.py
@@ -1,6 +1,7 @@
 from apscheduler.schedulers.tornado import TornadoScheduler
-from apscheduler.triggers.interval import IntervalTrigger  
+from apscheduler.triggers.interval import IntervalTrigger
 from tornado import httpclient
+from loguru import logger
 import feedparser
 
 global scheduler
@@ -17,21 +18,27 @@ class RSS_Source:
         self.id=param["id"]
         self.hash=0
         self.entries=[]
-    def update(self):
-        response = await httpclient.AsyncHTTPClient().fetch(self.url).body.decode(encoding="utf-8")
-        new_hash=hash(response)
-        if self.hash != new_hash:
-            # hash 不一致 RSS更新，准备群发
-            new_entries=feedparser.parse(response).entries
-            # post 要投递的 RSS 信息
-            post=[i for i in new_entries if i not in self.entries]
-            self.entries=new_entries
-            self.hash=new_hash
-            usr_list=self.bot.cilent.get_users_by_tag(str(self.id))
-            for usr in usr_list:
-                for i in post:
-                    # data 格式
-                    data={
+    async def update(self):
+       logger.info("开始检查:"+self.name)
+       response=await httpclient.AsyncHTTPClient().fetch(self.url)
+       logger.info("检查完成:"+self.name)
+       response=response.body.decode(encoding="utf-8")
+       new_hash=hash(response)
+       if self.hash != new_hash:
+           logger.info("检查到更新:"+self.name)
+           # hash 不一致 RSS更新，准备群发
+           new_entries=feedparser.parse(response).entries
+           # post 要投递的 RSS 信息
+           post=[i for i in new_entries if i not in self.entries]
+           self.entries=new_entries
+           self.hash=new_hash
+           usr_list=self.bot.client.get_users_by_tag(str(self.id))
+           if usr_list["count"]==0:
+               return
+           for usr in usr_list["data"]["openid"]:
+               for i in post:
+                   # data 格式
+                   data={
                     "first": {
                         "value": "订阅已更新"
                     },
@@ -39,27 +46,25 @@ class RSS_Source:
                         "value": i["title"]
                     },
                     "keyword2": {
-                        "value": "订阅"
+                        "value": self.name
                     },
                     "keyword3": {
-                        "value": i["summary"]
+                        "value":i["published"]  # "TIME HERE"
                     },
                     "keyword4": {
-                        "value": "南哪小助手"
+                        "value": "NJU"
                     },
                     "keyword5": {
-                        "value": "title"
+                        "value": i["summary"]
                     },
                     "remark": {
-                        "value": "测试模板消息"
+                        "value": "Powered by njuhelper-x"
                     },
-                }
-                    self.bot.client.send_template_message(usr,TEMPLATE,data,url="www.baidu.com")
-                pass
-        pass
+                      }
+                   self.bot.client.send_template_message(usr,TEMPLATE,data,url=i["link"])
     def run(self):
+        #预计最终版本是十分钟一更新
         scheduler.add_job(self.update,trigger=IntervalTrigger(minutes=10,jitter=10))
         pass
     def bind(self,bot):
         self.bot=bot
-        
